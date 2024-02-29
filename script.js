f59803562759574eed997d201458595490521a43
Yann Leyers <leyersyann@gmail.com>
2024-02-29 16:02:17 +0100

    initial commit + feedback

---
+++ script.js
@@ -0,0 +1,27 @@
+const cities = ['Mexico', 'Ukraine', 'Georgia', 'Morocco', 'Chile'];
+        const weather = document.querySelector('.weather');
+
+        async function getWeather(city) {
+            try {
+                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+                const data = await response.json();
+                return data.main.temp;
+            } catch (error) {
+                console.error('Error fetching weather data:', error);
+                return null;
+            }
+        }
+
+        async function updateSlider() {
+            weather.innerHTML = '';
+
+            for (const city of cities) {
+                const temperature = await getWeather(city);
+                const cityElement = document.createElement('div');
+                cityElement.textContent = `${city}: ${temperature}°C`;
+                weather.appendChild(cityElement);
+            }
+
+        }
+
+        updateSlider();
